5b711589fa9cb9b543bb3970f4ed4883e869b601
Timo <timo.w96@gmx.de>
2022-07-04 21:03:27 +0200

    Test

--- Tischreservierung/MainWindow.cs
+++ Tischreservierung/MainWindow.cs
@@ -25,19 +25,46 @@ namespace Tischreservierung
 
         private void ReserveTable_Click(object sender, EventArgs e)
         {
+                
+            if (!File.Exists(Globals.filepath))
+            {
+                var CreateFile = File.Create(Globals.filepath);
+                CreateFile.Close();
+                File.WriteAllText(Globals.filepath, "[");
+                MessageBox.Show("Eine neue Datei wurde erstellt");
+            }
+            else
+            { 
             
-            Reservation CreateReservation = new Reservation(textBoxFirstName.Text, textBoxSecondName.Text, dateTimePickerDate.Value.ToShortDateString(), comboBoxTable.Text, 5, textBoxPhoneNumber.Text);
-            string jsonString = JsonSerializer.Serialize(CreateReservation);
-            File.AppendAllText(Globals.filepath, jsonString);
+                if (new FileInfo(Globals.filepath).Length == 1)
+                {
+                    Reservation CreateReservation = new Reservation(textBoxFirstName.Text, textBoxSecondName.Text, dateTimePickerDate.Value.ToShortDateString(), comboBoxTable.Text, 5, textBoxPhoneNumber.Text);
+                    string jsonString = JsonSerializer.Serialize(CreateReservation);
+                    File.AppendAllText(Globals.filepath, jsonString);
+                    File.AppendAllText(Globals.filepath, "]");
+                    MessageBox.Show("Die Reservierung wurde erfolgreich erstellt!");
+                }
+
+                else
+                {
+                    string jsonStringRead = File.ReadAllText(Globals.filepath);
+                    var ReservationList = JsonSerializer.Deserialize<List<Reservation>>(jsonStringRead);
+                    Reservation CreateReservation = new Reservation(textBoxFirstName.Text, textBoxSecondName.Text, dateTimePickerDate.Value.ToShortDateString(), comboBoxTable.Text, 5, textBoxPhoneNumber.Text);
+                    ReservationList.Add(CreateReservation); 
+                    string jsonStringWrite = JsonSerializer.Serialize(ReservationList);
+                    File.WriteAllText(Globals.filepath, jsonStringWrite);
+                    MessageBox.Show("Die Reservierung wurde erfolgreich erstellt!");
+                }
+            }
 
         }
 
         private void ShowReservation_Click(object sender, EventArgs e)
         {
             string jsonString = File.ReadAllText(Globals.filepath);
-            Reservation ReadReservation = JsonSerializer.Deserialize<Reservation>(jsonString)!;
-            Console.WriteLine(ReadReservation);
-
+            var ReservationList = JsonSerializer.Deserialize<List<Reservation>>(jsonString);
+            Console.WriteLine(ReservationList);
+            
         }
     }
 }
